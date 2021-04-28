764bafc2037727d7946300ac2e5a6602e84bf163
konuleminova <konul.eminova39@gmail.com>
2021-04-28 12:43:26 +0400

    first commit

--- app/src/androidTest/java/com/example/hceapplicationdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/hceapplicationdemo/ExampleInstrumentedTest.kt
@@ -1,7 +1,6 @@
 package com.example.hceapplicationdemo
-
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
